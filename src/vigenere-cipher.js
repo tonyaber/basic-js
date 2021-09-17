474e42fd62615f27dbeca82a3e00566ce01924c9
Antonina Berchuk <tonyaberch@gmail.com>
2021-09-17 21:11:52 +0200

    feat: make task vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,24 +20,63 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt(message = null, key = null) {
-    if (message === null || key === null) {
-      throw new Error('Incorrect arguments!')
-    }
-    throw new NotImplementedError('Not implemented');
-
-
+  constructor(value=true) {
+    this.reverse = !value;
   }
-  decrypt(message = null, key = null) {
+
+  makeEncrypt(message, key, encrypt) {
     if (message === null || key === null) {
       throw new Error('Incorrect arguments!')
     }
-    throw new NotImplementedError('Not implemented');
+
+    let j = 0;
+    let cipher = '';
+
+    message = message.toLowerCase();
+    key = key.toLowerCase();
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].charCodeAt() < 97 || message[i].charCodeAt() > 122) {
+        cipher += message[i];
+        continue;
+      }
+
+      if (j >= key.length) {
+        j = 0;
+      }
+      
+      let countMessage = encrypt ? (message[i].charCodeAt() + key[j].charCodeAt() - 97)
+        : (message[i].charCodeAt() - (key[j].charCodeAt() - 97));
+      
+      if (encrypt) {
+        if (countMessage > 122) {
+          countMessage -= 26
+        }
+      } else {
+        if (countMessage < 97) {
+          countMessage += 26
+        }
+      }
+      
+      cipher += String.fromCharCode(countMessage).toUpperCase();
+      j++;
+    }
+
+    if (this.reverse) {
+      return cipher.split('').reverse().join('');
+    }
+    return cipher;
+  }
+
+  encrypt(message = null, key = null) {
+    return this.makeEncrypt(message, key, true)
+  }
+
+  decrypt(message = null, key = null) {
+    return this.makeEncrypt(message, key, false);
   }
 }
-//const directMachine = new VigenereCipheringMachine();
-//console.log(directMachine.encrypt('AEIHQX SX DLLU!', 'alphonse'))
-//const directMachine = new VigenereCipheringMachine();
-//console.log(directMachine.encrypt('AEIHQX SX DLLU!', 'alphonse'))
+const directMachine = new VigenereCipheringMachine();
+console.log(directMachine.encrypt('AEIHQX SX DLLU!', 'alphonse'))
 
 
